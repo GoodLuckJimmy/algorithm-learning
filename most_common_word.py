950b132a61ea353135659a374756abe5dc3fb82f
seokhan <seok.han@bringprice.com>
2020-11-23 13:24:45 +0900

    longest palindorome

--- most_common_word.py
+++ most_common_word.py
@@ -1,11 +1,9 @@
-
 import collections
 import re
 from typing import List
 
 def mostCommonWord(paragraph: str, banned: List[str]) -> str:
     words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
-    print(words)
     counts = collections.Counter(words)
     return counts.most_common(1)[0][0]
 
